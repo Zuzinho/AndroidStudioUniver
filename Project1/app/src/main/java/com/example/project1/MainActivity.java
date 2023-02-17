7e09755e74ec67d78c962c78b9bc9c482077b107
Zuzinho <95986354+Zuzinho@users.noreply.github.com>
2023-02-17 17:47:16 +0300

    Added toasts and logs in overridden methods on-

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toast.makeText(getApplicationContext(), "Created", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnCreate");
+
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "Started", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "Resumed", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "Paused", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "Destroyed", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "OnDestroy");
+    }
+
 }
