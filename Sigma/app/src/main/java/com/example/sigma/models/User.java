d60cc8635753fb6f8175a6be1f463124ffc650a5
Zuzinho <95986354+Zuzinho@users.noreply.github.com>
2023-02-24 03:12:40 +0300

    Created new class, user model

---
+++ Sigma/app/src/main/java/com/example/sigma/models/User.java
@@ -0,0 +1,37 @@
+package com.example.sigma.models;
+
+public final class User {
+    private final int id;
+    private String name;
+    private String position;
+    private String info;
+    private int AvatarPath;
+
+    public User(int id, String name, String position, String info, int avatarPath) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.info = info;
+        AvatarPath = avatarPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getAvatarPath() {
+        return AvatarPath;
+    }
+}
