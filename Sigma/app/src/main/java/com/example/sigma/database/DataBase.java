4401ee89ce8f0c3af3c8f8b10bc0bcc76bde1a12
Zuzinho <95986354+Zuzinho@users.noreply.github.com>
2023-02-24 03:11:53 +0300

    Created new class for simulating DataBase

---
+++ Sigma/app/src/main/java/com/example/sigma/database/DataBase.java
@@ -0,0 +1,23 @@
+package com.example.sigma.database;
+
+import com.example.sigma.R;
+import com.example.sigma.models.User;
+
+import java.util.ArrayList;
+
+public class DataBase {
+    public final static ArrayList<User> users = getUsers(10);
+
+    private static ArrayList<User> getUsers(int count){
+        ArrayList<User> arrayList = new ArrayList<>(count);
+        for(int i = 1;i < count + 1; i++)  arrayList.add(new User(i, "Name" + i, "Position" + i, "Info" + i, R.mipmap.user_avatar0));
+        return arrayList;
+    }
+
+    public static User getUserById(int userId){
+        for(User user: users){
+            if(user.getId() == userId) return user;
+        }
+        return null;
+    }
+}
