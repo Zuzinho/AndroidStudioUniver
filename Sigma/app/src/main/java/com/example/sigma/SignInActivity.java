0774bbdbd98b325e42d889449b15d6406f07344e
Zuzinho <95986354+Zuzinho@users.noreply.github.com>
2023-02-28 15:33:44 +0300

    Added new method omMainPageClicked in .java and onClick attribute in .xml

--- Sigma/app/src/main/java/com/example/sigma/SignInActivity.java
+++ Sigma/app/src/main/java/com/example/sigma/SignInActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -14,14 +15,6 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-        LinearLayout mainPageButton = (LinearLayout)findViewById(R.id.mainPageButton);
-        mainPageButton.setOnClickListener(v -> {
-            Log.i(TAG, "Clicked main page");
-
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
 
         Button signUpButton = (Button)findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(v -> {
@@ -40,5 +33,12 @@ public class SignInActivity extends AppCompatActivity {
             setResult(RESULT_OK, intent);
             finish();
         });
+    }
+
+    public void onMainPageClicked(View view){
+        Log.i(TAG, "Clicked main page");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- Sigma/app/src/main/java/com/example/sigma/SignUpActivity.java
+++ Sigma/app/src/main/java/com/example/sigma/SignUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -15,14 +16,6 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        LinearLayout mainPageButton = (LinearLayout)findViewById(R.id.mainPageButton);
-        mainPageButton.setOnClickListener(v -> {
-            Log.i(TAG, "Clicked main page");
-
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
-
         Button signInButton = (Button)findViewById(R.id.signInButton);
         signInButton.setOnClickListener(v -> {
             Log.i(TAG, "Clicked sign in button");
@@ -30,5 +23,12 @@ public class SignUpActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
         });
+    }
+
+    public void onMainPageClicked(View view){
+        Log.i(TAG, "Clicked main page");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
