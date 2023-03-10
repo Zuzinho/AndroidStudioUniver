075e050ed5f087260c3d4cae1cb2d38015887546
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-03-10 21:55:33 +0300

    Added OnClickListener for list_view_button button

--- Sigma/app/src/main/java/com/example/sigma/fragment/UserListSelectorFragment.java
+++ Sigma/app/src/main/java/com/example/sigma/fragment/UserListSelectorFragment.java
@@ -2,17 +2,36 @@ package com.example.sigma.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sigma.R;
+import com.example.sigma.database.DataBase;
 
 public class UserListSelectorFragment extends Fragment {
     public UserListSelectorFragment(){
         super(R.layout.fragment_user_list_selector);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_list_selector, container, false);
+
+        Button listViewSelectButton = view.findViewById(R.id.listViewSelectButton);
+        Button recyclerViewSelectButton = view.findViewById(R.id.recyclerViewSelectButton);
+
+        listViewSelectButton.setOnClickListener(v -> {
+            getParentFragmentManager().beginTransaction().replace(R.id.contentFragmentContainer,
+                    new UserListViewFragment(DataBase.users), "UserListViewFragment").commit();
+        });
+
+        return view;
+    }
 }
