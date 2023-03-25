b3bcf8fffe29b159186dfba7b6e00d224dc959b5
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-03-25 14:24:53 +0300

    Created service

---
+++ Sigma/app/src/main/java/com/example/sigma/MyService.java
@@ -0,0 +1,80 @@
+package com.example.sigma;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.location.SettingInjectorService;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.PixelCopy;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.sigma.activity.MainActivity;
+import com.example.sigma.activity.NotificationActivity;
+
+public class MyService extends Service {
+
+    private View mBannerView;
+    private WindowManager mWindowManager;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mBannerView = LayoutInflater.from(this).inflate(R.layout.banner_layout, null);
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        params.y = 200;
+
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent2 = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent2);
+        }
+        else {
+            mWindowManager.addView(mBannerView, params);
+        }
+
+        mBannerView.setOnClickListener(v -> {
+            Intent appIntent = new Intent(getApplicationContext(), MainActivity.class);
+            appIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(appIntent);
+            stopSelf();
+        });
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+
+        if(mBannerView != null){
+            mWindowManager.removeView(mBannerView);
+        }
+
+        super.onDestroy();
+    }
+}
