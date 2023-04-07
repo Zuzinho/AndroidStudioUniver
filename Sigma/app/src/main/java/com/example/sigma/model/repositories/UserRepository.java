24ccf6342d229021fcb13a715d5d27847baa678e
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-04-08 00:15:11 +0300

    Modified with changing UserDAo implementors

--- Sigma/app/src/main/java/com/example/sigma/model/repositories/UserRepository.java
+++ Sigma/app/src/main/java/com/example/sigma/model/repositories/UserRepository.java
@@ -1,22 +1,26 @@
 package com.example.sigma.model.repositories;
 
-import com.example.sigma.model.daos.userDAO.factory.UserDAOFactory;
+import com.example.sigma.model.daos.userDAO.FirstUserDAO;
+import com.example.sigma.model.daos.userDAO.SecondUserDAO;
 import com.example.sigma.model.models.User;
 
 public class UserRepository {
-    public User get(UserDAOFactory factory, int userId){
-        return factory.get().get(userId);
+    FirstUserDAO firstUserDAO = new FirstUserDAO();
+    SecondUserDAO secondUserDAO = new SecondUserDAO();
+
+    public User get(int userId){
+        return firstUserDAO.get(userId);
     }
 
-    public void create(UserDAOFactory factory, User user){
-        factory.get().create(user);
+    public void create(User user){
+        secondUserDAO.create(user);
     }
 
-    public void update(UserDAOFactory factory, User user){
-        factory.get().update(user);
+    public void update(User user){
+        firstUserDAO.update(user);
     }
 
-    public void delete(UserDAOFactory factory, User user){
-        factory.get().delete(user);
+    public void delete(User user){
+        secondUserDAO.delete(user);
     }
 }
