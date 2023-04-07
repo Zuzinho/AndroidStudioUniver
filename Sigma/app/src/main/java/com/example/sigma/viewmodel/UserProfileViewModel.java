552fa6addc8ca58a80aba4df132422ca505227fc
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-04-07 17:09:01 +0300

    Modified getting data with using repository

--- Sigma/app/src/main/java/com/example/sigma/viewmodel/UserProfileViewModel.java
+++ Sigma/app/src/main/java/com/example/sigma/viewmodel/UserProfileViewModel.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.sigma.model.daos.userDAO.factory.FirstUserUserDAOFactory;
 import com.example.sigma.model.models.User;
 import com.example.sigma.model.database.DataBase;
+import com.example.sigma.model.repositories.UserRepository;
 
 public class UserProfileViewModel extends ViewModel {
     private Context context;
@@ -16,7 +18,7 @@ public class UserProfileViewModel extends ViewModel {
     public void initViewModel(Context context, Intent intent){
         this.context = context;
         int userId = intent.getExtras().getInt("userId");
-        this.user = new MutableLiveData<>(DataBase.getUserById(userId));
+        this.user = new MutableLiveData<>(new UserRepository().get(new FirstUserUserDAOFactory(), userId));
     }
 
     public boolean existUser(){
