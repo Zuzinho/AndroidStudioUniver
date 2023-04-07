ccb6998908d3fd961a6235ed347be0f0f2b04d07
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-04-07 13:52:58 +0300

    Created new model for Projects

---
+++ Sigma/app/src/main/java/com/example/sigma/model/Project.java
@@ -0,0 +1,49 @@
+package com.example.sigma.model;
+
+public class Project {
+    private int id;
+    private String title;
+    private String photoUrl;
+    private String about;
+    private int userId;
+    private String technology;
+    private boolean selected;
+
+    public Project(int id, String title, String photoUrl, String about, int userId, String technology, boolean selected) {
+        this.id = id;
+        this.title = title;
+        this.photoUrl = photoUrl;
+        this.about = about;
+        this.userId = userId;
+        this.technology = technology;
+        this.selected = selected;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+}
