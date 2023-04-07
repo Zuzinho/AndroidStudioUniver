ba5cf36eca9258c6b4f2248205688013ef812322
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-04-07 13:52:13 +0300

    Craeted new model for Forms

---
+++ Sigma/app/src/main/java/com/example/sigma/model/Form.java
@@ -0,0 +1,31 @@
+package com.example.sigma.model;
+
+public class Form {
+    private int id;
+    private String email;
+    private String password;
+    private int recoverCode;
+
+    public Form(int id, String email, String password, int recoverCode) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.recoverCode = recoverCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getRecoverCode() {
+        return recoverCode;
+    }
+}
